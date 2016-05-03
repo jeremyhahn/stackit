af161617a7e884615ca124103f502f6aa619d51c
Jeremy Hahn <mail@jeremyhahn.com>
2016-05-03 11:03:07 -0700

    Add ability to resolve stack parameters from depends stacks

--- lib/stackit/stack/managed_stack_service.rb
+++ lib/stackit/stack/managed_stack_service.rb
@@ -3,42 +3,18 @@ module Stackit
   class ManagedStackService
 
     attr_accessor :options
+    attr_accessor :stacks
 
     def initialize(options)
       self.options = options
     end
 
     def create!
-      Stackit::ManagedStack.new(
-        template: template,
-        stack_name: stack_name,
-        stack_policy: options[:stack_policy],
-        depends: options[:depends],
-        user_defined_parameters: user_defined_parameters,
-        parameters_file: parameters_file,
-        parameter_map: parameter_mappings,
-        wait: options[:wait],
-        force: options[:force],
-        dry_run: options[:dry_run],
-        debug: !!options[:debug]
-      ).create!
+      stack.create!
     end
 
     def update!
-      ManagedStack.new(
-        template: template,
-        stack_name: stack_name,
-        stack_policy: options[:stack_policy],
-        stack_policy_during_update: options[:stack_policy_during_update],
-        depends: options[:depends],
-        user_defined_parameters: user_defined_parameters,
-        parameters_file: parameters_file,
-        parameter_map: parameter_mappings,
-        wait: options[:wait],
-        force: options[:force],
-        dry_run: options[:dry_run],
-        debug: !!options[:debug]
-      ).update!
+      stack.update!
     end
 
     def delete!
@@ -74,6 +50,42 @@ module Stackit
 
     def template_dir
       dir = options[:template_dir] ? options[:template_dir] : __dir__
+    end
+
+    def stack
+      ManagedStack.new(
+        template: template,
+        stack_name: stack_name,
+        stack_policy: options[:stack_policy],
+        stack_policy_during_update: options[:stack_policy_during_update],
+        depends: options[:depends],
+        user_defined_parameters: user_defined_parameters,
+        parameters_file: parameters_file,
+        parameter_map: parameter_mappings,
+        wait: options[:wait],
+        force: options[:force],
+        dry_run: options[:dry_run],
+        debug: !!options[:debug]
+      )
+    end
+
+    def depends_stacks
+      stacks = []
+      options[:depends].each do |stack|
+        stacks << Stackit::Stack.new(stack_name: stack)
+      end
+      stacks
+    end
+
+    def stacks
+      @stacks ||= [stack].concat(depends_stacks)
+    end
+
+    def resolve_parameter(key)
+      depends_stacks.each do |s|
+        value = s[key]
+        return value unless value.nil?
+      end
     end
 
   end
